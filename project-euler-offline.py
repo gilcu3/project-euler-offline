69351a1bd82f96cbde515ef8bff52fa9565ad0dd
John Shanahan <shanahan.jrs@gmail.com>
2015-01-11 13:15:49 -0800

    Moved cpu intense part of code to after you give answer. Makes starting the program almost instant.

--- project-euler-offline.py
+++ project-euler-offline.py
@@ -18,28 +18,27 @@ def loadJSON(jsonStr):
     return data
 
 def main():
+    eof = ["exit", "quit", "q", "c"]
     dir = os.path.dirname(__file__)
+
+    current = raw_input("What problem are you currently working on? ")
+    if current.lower() in eof: exit(0)
+
+
+    proposed = raw_input("Enter solution: ")
+
     txtFile = open(os.path.join(dir, "solutions-encrypted"), "r")
     txtStr = txtFile.read()
     txtFile.close()
     plain_text = triple_des('03b5660c7c16a07b').decrypt(txtStr, padmode=2)
     solutions = loadJSON(plain_text)
-    eof = ["exit", "quit", "q", "c"]
 
-    current = raw_input("What problem are you currently working on? ")
-    if current.lower() in eof: exit(0)
-
-    while True:
-        proposed = raw_input("\nEnter solution: ")
-        if proposed.lower() in eof:
-            break
-        elif proposed == solutions[current]:
-            print "Correct!"
-            current = raw_input("\nWhat problem are you working on? ")
-            if current == "exit":
-                break
-        else:
-            print "Sorry, but the answer you gave appears to be incorrect." 
+    if proposed.lower() in eof:
+        exit(0)
+    elif proposed == solutions[current]:
+        print "Correct!\n"
+    else:
+        print "Sorry, but the answer you gave appears to be incorrect." 
 
 if __name__ == "__main__":
     dir = os.path.dirname(__file__)
